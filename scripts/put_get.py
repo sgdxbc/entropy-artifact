c0bc403b84825e41cd2de0a62f470591c80a17d4
sgdxbc <sung@comp.nus.edu.sg>
2023-09-08 04:11:30 +0000

    Support more evaluation in benchmark script

--- scripts/put_get.py
+++ scripts/put_get.py
@@ -6,6 +6,8 @@ import random
 
 ARGV = dict(enumerate(sys.argv))
 NUM_OPERATION = int(ARGV.get(1, "1"))
+NUM_CONCURRENT = int(ARGV.get(2, "1"))
+assert NUM_CONCURRENT <= NUM_OPERATION
 PLAZA = "http://nsl-node1.d2:8080"
 
 
@@ -33,32 +35,48 @@ async def list_peer():
 
 async def put_get(peer):
     async with aiohttp.ClientSession() as session:
-        async with session.post(f'{peer}/benchmark/put') as resp:
+        async with session.post(f"{peer}/benchmark/put") as resp:
             put_id = await resp.json()
         while True:
             await asyncio.sleep(1)
-            async with session.get(f'{peer}/benchmark/put/{put_id}') as resp:
+            async with session.get(f"{peer}/benchmark/put/{put_id}") as resp:
                 result = await resp.json()
-                if result['put_end']:
+                if result["put_end"]:
                     break
-        latency = to_timestamp(result['put_end']) - to_timestamp(result['put_start'])
-        print(f'{peer},put,{latency}')
+        latency = to_timestamp(result["put_end"]) - to_timestamp(result["put_start"])
+        print(f"{peer},put,{latency}")
 
-        await session.post(f'{peer}/benchmark/get/{put_id}')
+        await session.post(f"{peer}/benchmark/get/{put_id}")
         while True:
             await asyncio.sleep(1)
-            async with session.get(f'{peer}/benchmark/put/{put_id}') as resp:
+            async with session.get(f"{peer}/benchmark/put/{put_id}") as resp:
                 result = await resp.json()
-                if result['get_end']:
+                if result["get_end"]:
                     break
-        latency = to_timestamp(result['get_end']) - to_timestamp(result['get_start'])
-        print(f'{peer},get,{latency}')
+        latency = to_timestamp(result["get_end"]) - to_timestamp(result["get_start"])
+        print(f"{peer},get,{latency}")
+
+
+async def operation(peers=None):
+    if not peers:
+        peers = await list_peer()
+    await put_get(random.choice(peers))
 
 
 async def main():
     peers = await list_peer()
-    peer = random.choice(peers)
-    await put_get(peer)
+    tasks = []
+    for _ in range(NUM_CONCURRENT):
+        tasks.append(operation(peers))
+    num_operation = NUM_CONCURRENT
+    while tasks:
+        done_tasks, tasks = await asyncio.wait(
+            tasks, return_when=asyncio.FIRST_COMPLETED
+        )
+        for _ in done_tasks:
+            if num_operation < NUM_OPERATION:
+                num_operation += 1
+                tasks.add(operation())
 
 
 if __name__ == "__main__":
